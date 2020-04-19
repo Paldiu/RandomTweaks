9d8e3e8fc4b80d29db4aa153d358e7effb6e8f5a
Paul Reilly <pawereus@gmail.com>
2020-04-19 15:59:00 -0400

    geg

--- src/main/java/ns/jovial/randomtweaks/reflect/Reflector.java
+++ src/main/java/ns/jovial/randomtweaks/reflect/Reflector.java
@@ -29,7 +29,7 @@ public class Reflector {
     }
 
     /**
-     * A basic reflection
+     * A basic reflection getter
      * @return A reflective interface
       */
     public Reflections reflect() {
